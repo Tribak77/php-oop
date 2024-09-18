45d01d2b831d07508a2fa7f66b4280d04c184f01
Tribak77 <atribak34@gmail.com>
2024-09-18 10:16:31 +0100

    tuto folder

---
+++ tuto/tuto1.php
@@ -0,0 +1,18 @@
+<?php
+class Voiture {
+    public $marque;
+    public $modele;
+    public $couleur;
+
+    public function demarrer() {
+        echo "La " . $this->couleur . " voiture démarre.";
+    }
+}
+
+// Création d'un objet Voiture
+$maVoiture = new Voiture();
+$maVoiture->marque = "Renault";
+$maVoiture->modele = "Clio";
+$maVoiture->couleur = "Bleu";
+
+$maVoiture->demarrer();
