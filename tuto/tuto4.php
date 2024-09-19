cf353d227d7118471a9088a7d8a6d33082b3a771
Tribak77 <atribak34@gmail.com>
2024-09-19 09:18:19 +0100

    tuto 4

---
+++ tuto/tuto4.php
@@ -0,0 +1,25 @@
+<?php
+class Animal {
+    public function faireDuBruit() {
+        echo "L'animal fait du bruit.";
+    }
+}
+
+class Chien extends Animal {
+    public function faireDuBruit() {
+        echo "Ouaf !";
+    }
+}
+
+class Chat extends Animal {
+    public function faireDuBruit() {
+        echo "Miaou !";
+    }
+}
+
+// Utilisation :
+$animaux = [new Chien(), new Chat(), new Animal()];
+
+foreach ($animaux as $animal) {
+    $animal->faireDuBruit();
+}
