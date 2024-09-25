da6a19ff1140553e7e83222924f0aae79d0429bb
Tribak77 <atribak34@gmail.com>
2024-09-25 09:42:43 +0100

    complet the presentation

--- Prototype/Presentation/BookPresentation.php
+++ Prototype/Presentation/BookPresentation.php
@@ -20,12 +20,45 @@ class BookPresentation{
             }
             echo "__________________________\n";
         } else{
+            echo "=========================\n";
             echo "No books available.\n";
         }
     }
 
-    public function addBooks(){
+    public function addBooks($title, $ISBD){
+        $BookService = new BookService();
+        $NewBook = new Book($title, $ISBD);
+        $BookService->addBooks($NewBook);
+        echo "Book added successfully:\n Title: " . $title . "\n" . "ISBD: ". $ISBD . "\n";
+    }
 
+    public function run(){
+        echo "====================";
+        echo "\nChoose an option\n";
+        echo "====================";
+
+        echo "\n[1]. view books\n";
+        echo "[2]. add a book\n";
+        echo "[3]. exit\n";
+        echo "--------------------\n";
+
+        $option = readline("Enter your choice: ");
+
+        switch($option){
+            case 1 :
+                $this->showBooks();
+                break;
+            case 2 :
+                $title = readline("Enter the title of the book: ");
+                $ISBD = readline("Enter the ISBD of the book: ");
+                $this->addBooks($title, $ISBD);
+                break; 
+            case 3 :
+                echo "Bye!\n";
+                exit;
+            default :
+            echo "Invalid choice. Please try again\n";           
+        }
     }
 }
 
